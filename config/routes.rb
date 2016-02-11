d5853086c86d356f1b8cbf446f1f88ecb1b6c779
Kenneth Ortiz <kenortiznj@gmail.com>
2016-02-11 03:55:45 +0000

    Added create action to places controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Nomster::Application.routes.draw do
   root 'places#index'
-
-  resources :places
   
+  resources :places
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
