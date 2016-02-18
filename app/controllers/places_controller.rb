870a4464b03d928cc49456e82e5e207b6f628ba7
Kenneth Ortiz <kenortiznj@gmail.com>
2016-02-18 03:43:59 +0000

    Hid Destroy button from other users. Added destroy to authenticate_user before_action. Added forbidden status if wrong user tries to destroy place.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
 		@places = Place.all
@@ -38,7 +38,11 @@ class PlacesController < ApplicationController
 	end
 
 	def destroy
-		@place = Place.find(param[:id])
+		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render text: 'Not Allowed', status: :forbidden
+		end
+
 		@place.destroy
 		redirect_to root_path
 	end
